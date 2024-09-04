61b47e2ee88a7af1ffcdf3ceae3a6321c65e249a
BinayaAdhikari <155567630+adhikaribijaya2035@users.noreply.github.com>
2024-09-05 01:46:20 +0545

    Update OperationButton.js

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,13 +1,3 @@
-// import React from 'react'
-// import { ACTIONS } from './App'
-
-// export default function OperationButtons({dispatch, operation}) {
-//   return (
-//    <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload:{operation}})}>{operation}
-//     </button>
-//   )
-// }
-
 
 import React from 'react';
 import { ACTIONS } from './App';
